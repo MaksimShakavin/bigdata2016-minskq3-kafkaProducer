aacd06eb0e2798ac0958579354491301b344287e
maksimshakavin <exelent.shock@gmail.com>
2016-09-27 10:31:56 +0300

    Add builder

--- src/main/java/com/epam/bigdata2016/minskq3/flume/TagsInterseptor.java
+++ src/main/java/com/epam/bigdata2016/minskq3/flume/TagsInterseptor.java
@@ -16,29 +16,29 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class TagsInterseptor implements Interceptor{
+public class TagsInterseptor implements Interceptor {
 
     private String vokPath;
     private String tagsHeader;
     private String hasHeaderTag;
-    private Map<String,String> vocabulary;
+    private Map<String, String> vocabulary;
 
-    public TagsInterseptor(String vokPath, String tagsHeader) {
+    public TagsInterseptor(String vokPath, String tagsHeader, String hasHeaderTag) {
         this.vokPath = vokPath;
         this.tagsHeader = tagsHeader;
+        this.hasHeaderTag = hasHeaderTag;
     }
 
     @Override
     public void initialize() {
-        try(Stream<String> lines = Files.lines(Paths.get(vokPath))){
+        try (Stream<String> lines = Files.lines(Paths.get(vokPath))) {
             lines
                     .map(line -> line.split("\\t"))
                     .collect(Collectors.toMap(
                             lineArr -> lineArr[0], //key -id
                             lineArr -> lineArr[1]  //value --tags string
                     ));
-        }
-        catch (IOException ex){
+        } catch (IOException ex) {
             throw new RuntimeIoException(ex);
         }
     }
@@ -47,12 +47,12 @@ public class TagsInterseptor implements Interceptor{
     public Event intercept(Event event) {
         Optional.of(event)
                 .map(e -> new String(e.getBody()).split("\\t"))
-                .map(arr -> arr[arr.length -2])
+                .map(arr -> arr[arr.length - 2])
                 .map(vocabulary::get)
-                .ifPresent(tags ->{
-                    Map<String,String> headers = event.getHeaders();
-                    headers.put("HAS_TAGS","true");
-                    event.getHeaders().put(tagsHeader,tags);
+                .ifPresent(tags -> {
+                    Map<String, String> headers = event.getHeaders();
+                    headers.put(hasHeaderTag, "true");
+                    event.getHeaders().put(tagsHeader, tags);
                 });
         return event;
     }
@@ -69,15 +69,21 @@ public class TagsInterseptor implements Interceptor{
 
     }
 
-    public static class Builderrr implements Interceptor.Builder{
+    public static class Builderrr implements Interceptor.Builder {
+        private String vokPath;
+        private String tagsHeader;
+        private String hasHeaderTag;
+
         @Override
         public Interceptor build() {
-            return null;
+            return new TagsInterseptor(vokPath, tagsHeader, hasHeaderTag);
         }
 
         @Override
         public void configure(Context context) {
-
+            vokPath = context.getString("vokPath");
+            tagsHeader = context.getString("tagsHeader");
+            hasHeaderTag = context.getString("hasHeaderTag");
         }
     }
 }
